3c3ce94f48576879af5b7862dde5bfbc476e9b45
bootstraponline <code@bootstraponline.com>
2014-03-18 19:34:58 -0400

    Add code

---
+++ BUCK
@@ -0,0 +1,8 @@
+java_test(
+  name = 'appium',
+  srcs = glob(['src/test/java/**/*.java']),
+  deps = [
+    '//lib:junit',
+  ],
+  visibility = ['PUBLIC'],
+)
---
+++ src/test/java/appium/Test.java
@@ -0,0 +1,10 @@
+package test.java.appium;
+
+import static org.junit.Assert.*;
+
+public class Test {
+    @org.junit.Test
+    public void testName() throws Exception {
+        assertEquals(1, 2);
+    }
+}
